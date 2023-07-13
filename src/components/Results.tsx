14fcc51d16ea143fbe00a3802c5668f703776963
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-07-13 15:54:06 -0300

    feat: new screenshot and changes result component

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -4,7 +4,8 @@ export function Results() {
       <div className="w-full max-w-xs mx-auto pt-7 pb-10 sm:max-w-[250px] sm:py-10">
         <h2>Your Results</h2>
         <p className="w-36 h-36 bg-gradient-to-b from-violet-blue to-persian-blue rounded-full mx-auto my-6 flex flex-col justify-center sm:w-48 sm:h-48">
-          <span className="text-white text-6xl font-bold">76</span> of 100
+          <span className="text-white text-6xl font-bold sm:text-7xl">76</span>{" "}
+          of 100
         </p>
         <h3 className="text-white text-3xl font-bold mb-4 sm:mb-6">Great</h3>
         <p>
