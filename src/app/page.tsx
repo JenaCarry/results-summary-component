5b648c5abb84daa36e1ab4be17752f5bce6ee0d2
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-07-13 15:46:45 -0300

    feat: changes page and results component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Summary } from "@/components/Summary";
 
 export default function Home() {
   return (
-    <main className="w-full shadow-sm sm:max-w-3xl sm:grid sm:grid-cols-2 sm:rounded-[2rem] sm:mx-2.5 sm:shadow-[4px_8px_25px_2px_rgba(46,43,233,0.1)]">
+    <main className="w-full shadow-sm sm:max-w-3xl sm:grid sm:grid-cols-2 sm:rounded-[2rem] sm:m-2.5 sm:shadow-[4px_8px_25px_2px_rgba(46,43,233,0.1)]">
       <h1 className="sr-only">Results summary component</h1>
       <Results />
       <Summary />
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -3,7 +3,7 @@ export function Results() {
     <section className="text-light-lavender text-center bg-gradient-to-b from-light-slate-blue to-light-royal-blue rounded-b-[2rem] px-8 sm:rounded-[2rem]">
       <div className="w-full max-w-xs mx-auto pt-7 pb-10 sm:max-w-[250px] sm:py-10">
         <h2>Your Results</h2>
-        <p className="w-36 h-36 bg-gradient-to-b from-violet-blue to-persian-blue rounded-full mx-auto my-6 flex flex-col justify-center sm:my-9">
+        <p className="w-36 h-36 bg-gradient-to-b from-violet-blue to-persian-blue rounded-full mx-auto my-6 flex flex-col justify-center sm:w-48 sm:h-48">
           <span className="text-white text-6xl font-bold">76</span> of 100
         </p>
         <h3 className="text-white text-3xl font-bold mb-4 sm:mb-6">Great</h3>
